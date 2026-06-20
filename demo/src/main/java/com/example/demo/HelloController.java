391fefdc817b9cd98822f04c7f720a822acc76aa
Adil ali <itsyou917@gmail.com>
2026-06-20 16:49:54 +0530

    first line of spring day01

---
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    
+@GetMapping("hello")
+public String hello()
+{
+    return "<h1>hello</h1>";
+}
+
+@GetMapping("/order")
+public String order() {
+    return "order is place";
+
+
+}
+
+
+
+}
